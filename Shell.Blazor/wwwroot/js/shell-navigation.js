a633a1f5dc62b10cdcffb3ecb9929f7a949ee2e3
Ing César Rolando Ortiz Rangel <cesar.ortiz@ksp.com.mx>
2026-03-12 03:18:25 -0600

    feat: shell-navigation added on app

---
+++ Shell.Blazor/wwwroot/js/shell-navigation.js
@@ -0,0 +1,8 @@
+﻿// wwwroot/js/shell-navigation.js
+window.listenMfeNavigation = function (dotnetRef) {
+    window.addEventListener('message', function (event) {
+        if (event.data && event.data.type === 'mfe-navigation') {
+            dotnetRef.invokeMethodAsync('OnMfeNavigated', event.data.path);
+        }
+    });
+};
