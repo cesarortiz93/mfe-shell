54ffb7833397648a5d7bab40e6ace833e698ea65
Ing César Rolando Ortiz Rangel <cesar.ortiz@ksp.com.mx>
2026-03-12 16:40:57 -0600

    fix: navigation

--- Shell.Blazor/wwwroot/js/shell-navigation.js
+++ Shell.Blazor/wwwroot/js/shell-navigation.js
@@ -1,7 +1,9 @@
 ﻿// wwwroot/js/shell-navigation.js
 window.listenMfeNavigation = function (dotnetRef) {
     window.addEventListener('message', function (event) {
+        console.log('Shell recibió mensaje:', event.data);
         if (event.data && event.data.type === 'mfe-navigation') {
+            console.log('Navegando MFE a:', event.data.path);
             dotnetRef.invokeMethodAsync('OnMfeNavigated', event.data.path);
         }
     });
